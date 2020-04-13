c1bc543fc95d4b4952d7910cf124876726c7ebd1
Efren Rama Maneiro <efren.rama.maneiro@usc.es>
2020-04-13 16:28:27 +0200

    Set allow growth
    Former-commit-id: 2e6381131181b5d60013b7fadfb37b8bc2695147

--- ImagePPMiner/run.py
+++ ImagePPMiner/run.py
@@ -26,6 +26,19 @@ parser.add_argument("--dataset", help="Raw dataset to prepare", required=True)
 arguments = parser.parse_args()
 dataset_name = Path(arguments.dataset).stem.split(".")[0].lower()
 
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
 # Create the model and output folders
 import os
 
