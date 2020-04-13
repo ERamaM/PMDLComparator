2db9d5efdd49df2bd32335cabbaee49d588a8b7f
Efren Rama Maneiro <efren.rama.maneiro@usc.es>
2020-04-13 13:20:14 +0200

    Add README.md
    Former-commit-id: cc534be8e3ece87512a0e10f9a41a1088772263c

---
+++ README.md
@@ -0,0 +1,55 @@
+# Installation
+
+Use anaconda to install some dependencies and activate the environment:
+
+    conda create -n "tf_2.0_ppm" python=3.6 tensorflow=2.1.0
+    conda activate tf_2.0_ppm
+
+Install additional dependencies:
+
+    python -m pip install pm4py==1.2.12 hyperopt==0.2.3
+    
+# Prepare the datasets
+
+There are two modes to prepare the datasets for training and testing: 
+
+- *Single file*: only a single log is processed (--dataset).
+- *Batch*: only the logs inside a folder are processed (--batch).
+
+To prepare the datasets you must specify to whom are you going to prepare the datasets for (with the argument --net).
+
+The structure is:
+
+    python dataset_processor.py --net AUTHOR --dataset LOG_XES_FILE
+    
+or:
+
+    python dataset_processor.py --net AUTHOR --batch FOLDER
+    
+Where AUTHOR is one of above:
+
+- pascuadibisceglie
+- mauro
+
+Examples:
+
+    python dataset_processor.py --net mauro --dataset raw_datasets/Helpdesk.xes.gz
+    
+    python dataset_processor.py --net mauro --batch raw_datasets
+    
+# Run the experiments
+
+## Pasquadibisceglie (ImagePPMiner)
+
+Run the experiments with the following command:
+
+    python run.py --dataset dataset/LOG_CSV_FILE
+    
+Where LOG_CSV_FILE are one log inside the "dataset" folder. 
+
+The results of the testing are outputted inside the "results" folder.
+
+The best model is inside the "models" folder.
+
+# Mauro (nnpm)
+
--- nnpm/deeppm_act.py
+++ nnpm/deeppm_act.py
@@ -1,11 +1,14 @@
 import numpy as np
+
 seed = 123
 np.random.seed(seed)
 from tensorflow.compat.v1 import set_random_seed
+
 set_random_seed(seed)
 
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Concatenate, Conv1D, GlobalAveragePooling1D, GlobalMaxPooling1D, Reshape, MaxPooling1D, Flatten, Dense, Embedding, Dropout
+from tensorflow.keras.layers import Input, Concatenate, Conv1D, GlobalAveragePooling1D, GlobalMaxPooling1D, Reshape, \
+    MaxPooling1D, Flatten, Dense, Embedding, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
@@ -19,11 +22,12 @@ import hyperopt
 from hyperopt.pyll.base import scope
 from hyperopt.pyll.stochastic import sample
 import sys
-import csv 
+import csv
 from time import perf_counter
 import time
 
 from tensorflow.keras.utils import Sequence
+
 
 class DataGenerator(Sequence):
     def __init__(self, features, labels, batch_size=32, shuffle=True):
@@ -43,7 +47,7 @@ class DataGenerator(Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
         # Generate data
         X, y = self.__data_generation(indexes)
         return X, y
@@ -55,14 +59,12 @@ class DataGenerator(Sequence):
             np.random.shuffle(self.indexes)
 
     def __data_generation(self, indexes):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
         X_a = np.empty((self.batch_size, self.X_a.shape[1]))
         X_t = np.empty((self.batch_size, self.X_t.shape[1]))
         y_a = np.empty((self.batch_size, self.y_a.shape[1]), dtype=int)
         y_t = np.empty((self.batch_size))
-           
-                       
 
         # Generate data
         for i, ID in enumerate(indexes):
@@ -73,13 +75,13 @@ class DataGenerator(Sequence):
             # Store class
             y_a[i] = self.y_a[ID]
             y_t[i] = self.y_t[ID]
-                       
 
-        return [X_a, X_t], {'output_a':y_a, 'output_t':y_t}
-    
+        return [X_a, X_t], {'output_a': y_a, 'output_t': y_t}
 
-def get_model(input_length=10, n_filters=3, vocab_size=10, n_classes=9, embedding_size=5, n_modules=5, model_type='ACT', learning_rate=0.002):
-    #inception model
+
+def get_model(input_length=10, n_filters=3, vocab_size=10, n_classes=9, embedding_size=5, n_modules=5, model_type='ACT',
+              learning_rate=0.002):
+    # inception model
 
     inputs = []
     for i in range(2):
@@ -87,7 +89,7 @@ def get_model(input_length=10, n_filters=3, vocab_size=10, n_classes=9, embeddin
 
     inputs_ = []
     for i in range(2):
-        if (i==0):
+        if (i == 0):
             a = Embedding(vocab_size, embedding_size, input_length=input_length)(inputs[0])
             inputs_.append(Embedding(vocab_size, embedding_size, input_length=input_length)(inputs[i]))
         else:
@@ -98,17 +100,17 @@ def get_model(input_length=10, n_filters=3, vocab_size=10, n_classes=9, embeddin
     for m in range(n_modules):
         filters = []
         for i in range(n_filters):
-            filters.append(Conv1D(filters=32, strides=1, kernel_size=1+i, activation='relu', padding='same')(filters_inputs))
+            filters.append(
+                Conv1D(filters=32, strides=1, kernel_size=1 + i, activation='relu', padding='same')(filters_inputs))
         filters.append(MaxPooling1D(pool_size=3, strides=1, padding='same')(filters_inputs))
         filters_inputs = Concatenate(axis=2)(filters)
-        #filters_inputs = Dropout(0.1)(filters_inputs)
+        # filters_inputs = Dropout(0.1)(filters_inputs)
 
-    #pool = GlobalAveragePooling1D()(filters_inputs)
+    # pool = GlobalAveragePooling1D()(filters_inputs)
     pool = GlobalMaxPooling1D()(filters_inputs)
-    #pool = Flatten()(filters_inputs)
+    # pool = Flatten()(filters_inputs)
 
-    #pool = Dense(64, activation='relu')(pool)
-
+    # pool = Dense(64, activation='relu')(pool)
 
     optimizer = Adam(lr=learning_rate)
 
@@ -116,13 +118,13 @@ def get_model(input_length=10, n_filters=3, vocab_size=10, n_classes=9, embeddin
         out_a = Dense(n_classes, activation='softmax', name='output_a')(pool)
         out_t = Dense(1, activation='linear', name='output_t')(pool)
         model = Model(inputs=inputs, outputs=[out_a, out_t])
-        model.compile(optimizer=optimizer, loss={'output_a':'categorical_crossentropy', 'output_t':'mae'})
+        model.compile(optimizer=optimizer, loss={'output_a': 'categorical_crossentropy', 'output_t': 'mae'})
     else:
-        if (model_type=='ACT'):
+        if (model_type == 'ACT'):
             out = Dense(n_classes, activation='softmax')(pool)
             model = Model(inputs=inputs, outputs=out)
-            model.compile(optimizer=optimizer, loss='mse',metrics=['acc'])
-        elif (model_type=='TIME'):
+            model.compile(optimizer=optimizer, loss='mse', metrics=['acc'])
+        elif (model_type == 'TIME'):
             out = Dense(1, activation='linear')(pool)
             model = Model(inputs=inputs, outputs=out)
             model.compile(optimizer=optimizer, loss='mae')
@@ -136,8 +138,10 @@ def fit_and_score(params):
     print(params)
     start_time = perf_counter()
 
-    model = get_model(input_length=params['input_length'], vocab_size=params['vocab_size'], n_classes=params['n_classes'], model_type=params['model_type'],
-                      learning_rate=params['learning_rate'], embedding_size=params['embedding_size'], n_modules=params['n_modules'])
+    model = get_model(input_length=params['input_length'], vocab_size=params['vocab_size'],
+                      n_classes=params['n_classes'], model_type=params['model_type'],
+                      learning_rate=params['learning_rate'], embedding_size=params['embedding_size'],
+                      n_modules=params['n_modules'])
     early_stopping = EarlyStopping(monitor='val_loss', patience=20)
     """
     num_train = int(X_a_train.shape[0]*0.8)
@@ -156,21 +160,20 @@ def fit_and_score(params):
     train_generator = DataGenerator([X_a_train1,X_t_train1], [y_a_train1,y_t_train1], batch_size=2**params['batch_size'])
     val_generator = DataGenerator([X_a_val1,X_t_val1], [y_a_val1,y_t_val1], batch_size=2**params['batch_size'])
     """
-   
+
     if (params['model_type'] == 'ACT'):
         h = model.fit([X_a_train, X_t_train],
-                      y_a_train, epochs=200, verbose=0, 
-                      validation_split=0.2, callbacks=[early_stopping], batch_size=2**params['batch_size'])
+                      y_a_train, epochs=200, verbose=0,
+                      validation_split=0.2, callbacks=[early_stopping], batch_size=2 ** params['batch_size'])
     elif (params['model_type'] == 'TIME'):
         h = model.fit([X_a_train, X_t],
-                      y_t_train, epochs=200, 
-                      validation_split=0.2, callbacks=[early_stopping], batch_size=2**params['batch_size'])
+                      y_t_train, epochs=200,
+                      validation_split=0.2, callbacks=[early_stopping], batch_size=2 ** params['batch_size'])
     else:
-         h = model.fit([X_a_train, X_t_train],
-                      {'output_a':y_a_train, 'output_t':y_t_train}, epochs=200, verbose=0,
-                      validation_split=0.2, callbacks=[early_stopping], batch_size=2**params['batch_size'])
-#        h = model.fit_generator(generator=train_generator, validation_data=val_generator, use_multiprocessing=True, workers=8, epochs=200, callbacks=[early_stopping], max_queue_size=10000, verbose=0)
-
+        h = model.fit([X_a_train, X_t_train],
+                      {'output_a': y_a_train, 'output_t': y_t_train}, epochs=200, verbose=0,
+                      validation_split=0.2, callbacks=[early_stopping], batch_size=2 ** params['batch_size'])
+    #        h = model.fit_generator(generator=train_generator, validation_data=val_generator, use_multiprocessing=True, workers=8, epochs=200, callbacks=[early_stopping], max_queue_size=10000, verbose=0)
 
     scores = [h.history['val_loss'][epoch] for epoch in range(len(h.history['loss']))]
     score = min(scores)
@@ -185,7 +188,8 @@ def fit_and_score(params):
         best_numparameters = model.count_params()
         best_time = end_time - start_time
 
-    return {'loss': score, 'status': STATUS_OK,  'n_epochs':  len(h.history['loss']), 'n_params':model.count_params(), 'time':end_time - start_time}
+    return {'loss': score, 'status': STATUS_OK, 'n_epochs': len(h.history['loss']), 'n_params': model.count_params(),
+            'time': end_time - start_time}
 
 
 logfile = sys.argv[1]
@@ -205,22 +209,20 @@ outfile.write("Starting time: %s\n" % current_time)
  divisor,
  prefix_sizes) = load_data(logfile)
 
-emb_size = (vocab_size + 1 ) // 2 # --> ceil(vocab_size/2)
+emb_size = (vocab_size + 1) // 2  # --> ceil(vocab_size/2)
 
 # normalizing times
 X_t = X_t / np.max(X_t)
 # categorical output
 y_a = to_categorical(y_a)
 
-
 n_iter = 20
 
-space = {'input_length':max_length, 'vocab_size':vocab_size, 'n_classes':n_classes, 'model_type':model_type, 'embedding_size':emb_size,
-         'n_modules':hp.choice('n_modules', [1,2,3]),
-         'batch_size': hp.choice('batch_size', [9,10]),
+space = {'input_length': max_length, 'vocab_size': vocab_size, 'n_classes': n_classes, 'model_type': model_type,
+         'embedding_size': emb_size,
+         'n_modules': hp.choice('n_modules', [1, 2, 3]),
+         'batch_size': hp.choice('batch_size', [9, 10]),
          'learning_rate': hp.loguniform("learning_rate", np.log(0.00001), np.log(0.01))}
-
-
 
 final_brier_scores = []
 final_accuracy_scores = []
@@ -230,17 +232,17 @@ for f in range(3):
     print("Fold", f)
     outfile.write("\nFold: %d" % f)
     # split into train and test set
-    p = np.random.RandomState(seed=seed+f).permutation(X_a.shape[0])
-    elems_per_fold = int(round(X_a.shape[0]/3))
+    p = np.random.RandomState(seed=seed + f).permutation(X_a.shape[0])
+    elems_per_fold = int(round(X_a.shape[0] / 3))
 
-    X_a_train = X_a[p[:2*elems_per_fold]]
-    X_t_train = X_t[p[:2*elems_per_fold]]
-    X_a_test = X_a[p[2*elems_per_fold:]]
-    X_t_test = X_t[p[2*elems_per_fold:]]
-    y_a_train = y_a[p[:2*elems_per_fold]]
-    y_a_test = y_a[p[2*elems_per_fold:]]
-    y_t_train = y_t[p[:2*elems_per_fold]]
-    y_t_test = y_t[p[2*elems_per_fold:]]
+    X_a_train = X_a[p[:2 * elems_per_fold]]
+    X_t_train = X_t[p[:2 * elems_per_fold]]
+    X_a_test = X_a[p[2 * elems_per_fold:]]
+    X_t_test = X_t[p[2 * elems_per_fold:]]
+    y_a_train = y_a[p[:2 * elems_per_fold]]
+    y_a_test = y_a[p[2 * elems_per_fold:]]
+    y_t_train = y_t[p[:2 * elems_per_fold]]
+    y_t_test = y_t[p[2 * elems_per_fold:]]
 
     # model selection
     print('Starting model selection...')
@@ -250,31 +252,33 @@ for f in range(3):
     best_numparameters = 0
 
     trials = Trials()
-    best = fmin(fit_and_score, space, algo=tpe.suggest, max_evals=n_iter, trials=trials, rstate= np.random.RandomState(seed+f))
+    best = fmin(fit_and_score, space, algo=tpe.suggest, max_evals=n_iter, trials=trials,
+                rstate=np.random.RandomState(seed + f))
     best_params = hyperopt.space_eval(space, best)
 
     outfile.write("\nHyperopt trials")
     outfile.write("\ntid,loss,learning_rate,n_modules,batch_size,time,n_epochs,n_params,perf_time")
     for trial in trials.trials:
-        outfile.write("\n%d,%f,%f,%d,%d,%s,%d,%d,%f"%(trial['tid'],
-                                                trial['result']['loss'],
-                                                trial['misc']['vals']['learning_rate'][0],
-                                                int(trial['misc']['vals']['n_modules'][0]+1),
-                                                trial['misc']['vals']['batch_size'][0]+7,
-                                                (trial['refresh_time']-trial['book_time']).total_seconds(),
-                                                trial['result']['n_epochs'],
-                                                trial['result']['n_params'],
-                                                trial['result']['time']))
+        outfile.write("\n%d,%f,%f,%d,%d,%s,%d,%d,%f" % (trial['tid'],
+                                                        trial['result']['loss'],
+                                                        trial['misc']['vals']['learning_rate'][0],
+                                                        int(trial['misc']['vals']['n_modules'][0] + 1),
+                                                        trial['misc']['vals']['batch_size'][0] + 7,
+                                                        (trial['refresh_time'] - trial['book_time']).total_seconds(),
+                                                        trial['result']['n_epochs'],
+                                                        trial['result']['n_params'],
+                                                        trial['result']['time']))
 
     outfile.write("\n\nBest parameters:")
     print(best_params, file=outfile)
     outfile.write("\nModel parameters: %d" % best_numparameters)
-    outfile.write('\nBest Time taken: %f'%best_time)
+    outfile.write('\nBest Time taken: %f' % best_time)
 
     # evaluate
     print('Evaluating final model...')
-    preds_a = best_model.predict([X_a_test,X_t_test])
-    brier_score = np.mean(list(map(lambda x: brier_score_loss(y_a_test[x],preds_a[x]),[i[0] for i in enumerate(y_a_test)])))
+    preds_a = best_model.predict([X_a_test, X_t_test])
+    brier_score = np.mean(
+        list(map(lambda x: brier_score_loss(y_a_test[x], preds_a[x]), [i[0] for i in enumerate(y_a_test)])))
 
     y_a_test = np.argmax(y_a_test, axis=1)
     preds_a = np.argmax(preds_a, axis=1)
@@ -287,8 +291,6 @@ for f in range(3):
     final_accuracy_scores.append(accuracy)
 
     outfile.write(np.array2string(confusion_matrix(y_a_test, preds_a), separator=", "))
-    
-
 
     outfile.flush()
 
