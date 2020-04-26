4d101982b7766b32b11667618f83812c6f6ba6ba
Efren Rama Maneiro <efren.rama.maneiro@usc.es>
2020-04-26 17:28:17 +0200

    Fix script.
    Former-commit-id: e81720a01e6d5dd913f6f4f2898f5d9f1453e538

--- nnpm/generate_queue.sh
+++ nnpm/generate_queue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(ls data | grep -v "train" | grep -v "val" | grep -v "test"); do
-	tsp python deepm_act.py data/"$i" ACT results/"${$i%%.*}_results.log"
+	tsp python deepm_act.py data/"$i" ACT results/"${i%%.*}_results.log"
 done
