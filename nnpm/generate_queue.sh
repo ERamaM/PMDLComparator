41fe9dd6e979455d4fbb5bc3d2c62cb8d0c124fe
Efren Rama Maneiro <efren.rama.maneiro@usc.es>
2020-04-27 11:09:50 +0200

    Clean results directory before experimenting.
    Former-commit-id: 3dab3015e76bf0fc7602c7d59d2faea02b5d885a

--- nnpm/generate_queue.sh
+++ nnpm/generate_queue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm results/*
 for i in $(ls data | grep -v "train" | grep -v "val" | grep -v "test"); do
-	tsp python deepm_act.py data/"$i" ACT results/"${i%%.*}_results.log"
+	tsp python deeppm_act.py data/"$i" ACT results/"${i%%.*}_results.log"
 done
