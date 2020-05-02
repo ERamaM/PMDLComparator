49d14af22ab12fe0b61c589f879780b721dd01eb
Efren Rama Maneiro <efren.rama.maneiro@usc.es>
2020-05-02 15:35:37 +0200

    Fix scripts.
    Former-commit-id: a7c22f213a941b88477fa18c0ee383be5f7eb1fe

--- ImagePPMiner/generate_queue.sh
+++ ImagePPMiner/generate_queue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm results/*
 for i in $(ls dataset | grep -v "train" | grep -v "val" | grep -v "test"); do
-	tsp python run.py --dataset dataset/$i
+	tsp python run.py --dataset dataset/$i --train --test
 done
