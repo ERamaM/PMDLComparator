d0b24aa874aa2a5285388396356e212488b2d5b6
Efren Rama Maneiro <efren.rama.maneiro@usc.es>
2020-05-11 21:23:28 +0200

    Change script
    Former-commit-id: 036e72b29173ce5ee400e9e5357dd9b0ab61ec0e

--- PyDREAM-NAP/generate_queue.sh
+++ PyDREAM-NAP/generate_queue.sh
@@ -4,6 +4,6 @@ rm output_models/*
 rm best_models/*
 N_THREADS=8
 for i in $(ls logs | grep -v "train" | grep -v "val" | grep -v "test"); do
-  tsp java -jar splitminer_cmd-1.0.0-all.jar -l logs/"train_val_"$i -m output_models -b best_models -t $N_THREADS
+  tsp java -jar splitminer_cmd-1.0.0-all.jar -l ./logs/"train_val_"$i -m output_models -b best_models -t $N_THREADS
 	tsp python run_dreamnap.py --dataset logs/"$i" --train --test
 done
