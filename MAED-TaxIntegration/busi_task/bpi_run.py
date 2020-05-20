bf4460cb7510e8dfd5b7070cf7b08e85ca8f81e0
efren.rama <efren.rama.maneiro@usc.es>
2020-05-20 10:59:17 +0200

    Add gpu utilization script
    Former-commit-id: 2638ec995c3a798c371df225661996c81d63bb51

--- MAED-TaxIntegration/busi_task/bpi_run.py
+++ MAED-TaxIntegration/busi_task/bpi_run.py
@@ -862,6 +862,17 @@ def exact_bpi_test_mix(
 
 
 if __name__ == '__main__':
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
     # So, here is the deal.
     # The original code loads a set of pickles that contain the already preprocessed dataset.
     # We assume that the preprocessing matches the same as used by Tax et al.
