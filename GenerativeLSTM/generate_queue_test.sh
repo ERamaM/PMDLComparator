f128b01faa9bd920daf7fafeb7ec52f8201729e4
efren.rama <efren.rama.maneiro@usc.es>
2020-05-23 17:12:27 +0200

    Delete spurious line.
    Former-commit-id: 70ce60282e018829f5ec3147d0e8a01d8d8cf1bf

--- GenerativeLSTM/generate_queue_test.sh
+++ GenerativeLSTM/generate_queue_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 TSP=ts
-rm -r ./output_files/*
 for i in $(ls input_files | grep -v "train" | grep -v "val" | grep -v "test" | grep -v "embedded_matix"); do
   $TSP python evaluation_generator.py --log $i
 done
