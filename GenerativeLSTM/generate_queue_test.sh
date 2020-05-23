6a2018a86e9776a01234f6052e84a94a64850cfa
efren.rama <efren.rama.maneiro@usc.es>
2020-05-23 17:11:08 +0200

    Fix queue test
    Former-commit-id: 43d3b55fd42027f7c3a158022520f2502ae6ddc9

--- GenerativeLSTM/generate_queue_test.sh
+++ GenerativeLSTM/generate_queue_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TSP=ts
 rm -r ./output_files/*
-for i in $(ls input_files | grep -v "train" | grep -v "val" | grep -v "test"); do
-  $TSP python evaluation_generator.py --log input_files/$i
+for i in $(ls input_files | grep -v "train" | grep -v "val" | grep -v "test" | grep -v "embedded_matix"); do
+  $TSP python evaluation_generator.py --log $i
 done
