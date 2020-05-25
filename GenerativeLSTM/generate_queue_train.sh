eb4d8b2000689a467cd7ea74055559421e6d906f
efren.rama <efren.rama.maneiro@usc.es>
2020-05-25 19:08:52 +0200

    Partially disable the multiprocessing in tsp
    Former-commit-id: d3148b0e4ece47e8371061964abc374af17f3877

--- GenerativeLSTM/generate_queue_train.sh
+++ GenerativeLSTM/generate_queue_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -r ./output_files/*
-N_SLOTS=3
+N_SLOTS=1
 for i in $(ls input_files | grep -v "train" | grep -v "val" | grep -v "test" | grep -v "embedded_matix"); do
   python experiment_generator.py --log input_files/$i --execute_inplace --slots $N_SLOTS
 done
