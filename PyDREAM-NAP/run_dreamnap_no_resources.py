4e44e1df0e2ba9f636d216699e9f32fea72061ee
efren.rama <efren.rama.maneiro@usc.es>
2020-05-28 11:54:33 +0200

    Add dream-nap without resources.
    Former-commit-id: 45c3b557b082726e6a4e03df7018017624e2fa00

---
+++ PyDREAM-NAP/run_dreamnap_no_resources.py
@@ -0,0 +1,123 @@
+from pm4py.objects.log.importer.xes import factory as xes_import_factory
+from pm4py.objects.petri.importer import pnml as pnml_importer
+from pm4py.algo.discovery.heuristics import factory as heuristics_miner
+from pm4py.objects.petri.exporter import pnml as pnml_exporter
+from DREAM.pydream.EnhancedPN import EnhancedPN
+from DREAM.pydream import LogWrapper
+import os
+import json
+import re
+
+from DREAM.pydream.LogWrapper import LogWrapper
+from DREAM.pydream.predictive.nap.NAP import NAP
+from DREAM.pydream.predictive.nap.NAPr import NAPr
+
+import tensorflow as tf
+import numpy as np
+import argparse
+import yaml
+from pathlib import Path
+
+enhanced_pn_folder = "./enhanced_pns_no_resources/"
+best_model_folder = "./best_models/"
+# Beware of the last slash. NAP does not use os.path.join
+model_checkpont_folder = "./model_checkpoints_no_resources"
+results_folder = "./results_no_resources"
+
+# Avoid saturating the GPU memory
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+tf.random.set_seed(42)
+np.random.seed(42)
+
+parser = argparse.ArgumentParser(description="Run the neural net")
+parser.add_argument("--dataset", type=str, required=True)
+parser.add_argument("--train", help="Start training the neural network", action="store_true")
+parser.add_argument("--test", help="Start testing next event of the neural network", action="store_true")
+args = parser.parse_args()
+
+dataset_path = args.dataset
+dataset_directory = Path(dataset_path).parent
+log_name = Path(dataset_path).name
+
+model_regex = "train_val_" + log_name + "_\d\.\d_\d\.\d\.pnml"
+model_regex_logs = "logs_" + model_regex
+train_log_file = "./logs/train_" + log_name
+val_log_file = "./logs/val_" + log_name
+test_log_file = "./logs/test_" + log_name
+total_log_file = "./logs/" + log_name
+
+main_log = xes_import_factory.apply(dataset_path)
+main_log = LogWrapper(main_log)
+
+model_file = None
+for file in os.listdir(best_model_folder):
+    if re.match(model_regex, file):
+        model_file = file
+        break
+    if re.match(model_regex_logs, file):
+        model_file = file
+        break
+
+if model_file is None:
+    raise FileNotFoundError("Unable to find mined model. Have you executed the splitminer?")
+
+net, initial_marking, final_marking = pnml_importer.import_net(best_model_folder + model_file)
+
+
+def load_and_process(log_file, type):
+    log = xes_import_factory.apply(log_file)
+
+    log_wrapper = LogWrapper(log)
+    enhanced_pn = EnhancedPN(net, initial_marking)
+    enhanced_pn.enhance(log_wrapper)
+
+    timedstatesamples_json, tss_objs = enhanced_pn.decay_replay(log_wrapper=log_wrapper)
+
+    save_json_path = enhanced_pn_folder + type + log_name + "_timedstatesamples.json"
+
+    with open(save_json_path, "w") as f:
+        json.dump(timedstatesamples_json, f)
+
+    return save_json_path
+
+
+# Find the best model corresponding to the log
+if not os.path.isdir(enhanced_pn_folder):
+    os.mkdir(enhanced_pn_folder)
+
+full_file = load_and_process(dataset_path, "")
+train_file = load_and_process(train_log_file, "train_")
+val_file = load_and_process(val_log_file, "val_")
+test_file = load_and_process(test_log_file, "test_")
+
+# If there is no attribute
+nap = NAP(tss_train_file=train_file, tss_full_log=full_file, tss_val_file=val_file, tss_test_file=test_file, options={"n_epochs": 100})
+
+if not os.path.isdir(model_checkpont_folder):
+    os.mkdir(model_checkpont_folder)
+
+if args.train:
+    nap.train(checkpoint_path=model_checkpont_folder, name=log_name, save_results=True)
+if args.test:
+    nap.loadModel(model_checkpont_folder, log_name)
+    if not os.path.exists(results_folder):
+        os.mkdir(results_folder)
+    nap.perform_test(
+        os.path.join(results_folder, log_name + "_results.txt"),
+        os.path.join(results_folder, "raw_" + log_name + ".txt")
+    )
+
+
+"""
+enhanced_pn.saveToFile(enhanced_pn_folder + log_name + ".json")
+"""
