77652692ebfa4a31291659daa424c871204c324e
Efrén Rama Maneiro <efren.rama.maneiro@usc.es>
2021-06-24 17:05:16 +0200

    Modify evermann script
    Former-commit-id: 7e6ca5b33ea7a4cbcfca92b14bc4cdb0bc88c2c8

--- evermann/generate_queue.sh
+++ evermann/generate_queue.sh
@@ -12,5 +12,5 @@ for i in $(ls data | grep  "train"); do
 	  #tsp python train.py --dataset data/$i --train --test --test_suffix
 	  log=${i/train_/}
 	  full_log=${i/train_fold[0-9]_variation[0-9]_/}
-	  TS_SOCKET=/tmp/evermann $TS_EXECUTABLE python train.py --fold_dataset data/$log --full_dataset data/$full_log --train --test --test_suffix
+	  CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/evermann $TS_EXECUTABLE python train.py --fold_dataset data/$log --full_dataset data/$full_log --train --test --test_suffix --test_suffix_calculus
 done
