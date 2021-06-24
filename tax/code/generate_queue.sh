2e971626391b8a5b51a5ac848e42c3f2e6c5e69d
efren.rama <efren.rama.maneiro@usc.es>
2021-06-24 16:19:45 +0200

    Tax script.
    Former-commit-id: e1db0ce4652fe617de3413479ae9b8423bf032fc

--- tax/code/generate_queue.sh
+++ tax/code/generate_queue.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 rm results/*
-for i in $(ls ../data | grep -v "train" | grep -v "val" | grep -v "test"); do
-	tsp python train.py --dataset ../data/"$i" --train --test --test_suffix --test_suffix_calculus
+if command -v ts &> /dev/null
+then
+    TS_EXECUTABLE=ts
+else
+    TS_EXECUTABLE=tsp
+fi
+
+for i in $(ls ../data | grep  "train"); do
+	  log=${i/train_/}
+	  full_log=${i/train_fold[0-9]_variation[0-9]_/}
+	  TS_SOCKET=/tmp/tax $TS_EXECUTABLE python train.py --fold_dataset ../data/$log --full_dataset ../data/$full_log --train --test --test_suffix --test_suffix_calculus
 done
