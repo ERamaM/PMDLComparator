c3b80aab5fa939e74a2b64d46652c7072236011d
efren.rama <efren.rama.maneiro@usc.es>
2021-06-24 18:50:08 +0200

    Refine evermann script.
    Former-commit-id: 9311b1d5fa8845ca59048bf0d9c4b987c2518c6a

--- evermann/generate_queue.sh
+++ evermann/generate_queue.sh
@@ -11,6 +11,7 @@ fi
 current_host=`hostname`
 DUMP_DIR=/home/efren.rama/tmp_hot_garbage/
 
+# Prepare to dump tmp files in other folder.
 if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
 then
   mkdir -p $DUMP_DIR
@@ -22,7 +23,7 @@ for i in $(ls data | grep  "train"); do
 	  full_log=${i/train_fold[0-9]_variation[0-9]_/}
 	  if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
 	  then
-	    TMP_DIR=$DUMP_DIR CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/evermann $TS_EXECUTABLE python train.py --fold_dataset data/$log --full_dataset data/$full_log --train --test --test_suffix --test_suffix_calculus
+	    TMPDIR=$DUMP_DIR CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/evermann $TS_EXECUTABLE python train.py --fold_dataset data/$log --full_dataset data/$full_log --train --test --test_suffix --test_suffix_calculus
 	  else
 	    CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/evermann $TS_EXECUTABLE python train.py --fold_dataset data/$log --full_dataset data/$full_log --train --test --test_suffix --test_suffix_calculus
 	  fi
