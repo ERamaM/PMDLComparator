29847e3bccd0b923122cc2874b293efb9c512ae8
Ubuntu <citius@DioBrando.efren.rama>
2021-07-07 11:34:28 +0000

    Disable training to search only models.
    Former-commit-id: 5340e094d7d2368ce940f997e916cbdf60164f6e

--- PyDREAM-NAP/generate_queue.sh
+++ PyDREAM-NAP/generate_queue.sh
@@ -24,13 +24,13 @@ for i in $(ls logs | grep "train" | grep -v "val"); do
 	  log=${i/train_/}
 	  full_log=${i/train_fold[0-9]_variation[0-9]_/}
 	  train_val_log="train_val_"$log
-  	TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_splitminer.py --log ./logs/$train_val_log --output_folder output_models --best_model best_models --n_threads $N_THREADS
-	  if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
-	  then
-	    TMPDIR=$DUMP_DIR CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap.py --fold_dataset logs/$log--full_dataset logs/$full_log --train --test
-	    TMPDIR=$DUMP_DIR CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap_no_resources.py --fold_dataset logs/$log --full_dataset logs/$full_log --train --test
-	  else
-	    TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap.py --fold_dataset logs/$log --full_dataset logs/$full_log --train --test
-	    TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap_no_resources.py --fold_dataset logs/$log --full_dataset logs/$full_log  --train --test
-	  fi
+  	TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_splitminer_new_version.py --log ./logs/$train_val_log --output_folder output_models --best_model best_models --n_threads $N_THREADS
+	  #if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
+	  #then
+	  #  TMPDIR=$DUMP_DIR CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap.py --fold_dataset logs/$log--full_dataset logs/$full_log --train --test
+	  #  TMPDIR=$DUMP_DIR CUDA_VISIBLE_DEVICES=0 TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap_no_resources.py --fold_dataset logs/$log --full_dataset logs/$full_log --train --test
+	  #else
+	  #  TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap.py --fold_dataset logs/$log --full_dataset logs/$full_log --train --test
+	  #  TS_SOCKET=/tmp/theis $TS_EXECUTABLE python run_dreamnap_no_resources.py --fold_dataset logs/$log --full_dataset logs/$full_log  --train --test
+	  #fi
 done
