7b0cf3f0dbce0bef09c40d5cabe984abc70b6cca
Efrén Rama Maneiro <efren.rama.maneiro@usc.es>
2021-07-16 10:53:36 +0200

    Add times camargo
    Former-commit-id: d436ba261c2345e704b92337c91d4ffc87adf171

--- results_processor/time_processor/time_task_spooler.py
+++ results_processor/time_processor/time_task_spooler.py
@@ -33,18 +33,30 @@ if ts:
         TS_EXECUTABLE = "tsp"
 
     # https://stackoverflow.com/questions/42201691/reading-a-complicated-table-with-pandas-task-spooler
-    ts_command = "TS_SOCKET=/tmp/" + approach + " " + TS_EXECUTABLE + " | sed -e 's/\s\{2,12\}/,/g' | sed -e 's/\([[:digit:]].[[:digit:]]\{2\}\) /\1,/' "
+    #ts_command = "TS_SOCKET=/tmp/" + approach + " " + TS_EXECUTABLE + " | sed -e 's/\s\{2,12\}/,/g' | sed -e 's/\([[:digit:]].[[:digit:]]\{2\}\) /\1,/' "
+    #ts_command = "TS_SOCKET=/tmp/" + approach + " " + TS_EXECUTABLE + " | sed -e 's/\s\{2,8\}/,/g' | sed -e 's/\([[:digit:]].[[:digit:]]\{2\}\) /\1,/' | sed -e 's/,\([[:digit:]]\+.[[:digit:]]\{2\}\)/,,\1/'"
+    """
+    ts_command = "TS_SOCKET=/tmp/" + approach + " " + TS_EXECUTABLE
     csv_str = subprocess.check_output(ts_command, shell=True)
-    csv_str = csv_str.decode("utf-8")
+    #print("CSV Str regular: ", csv_str)
+    csv_str = csv_str.decode("ANSI_X3.4-1968")
+    print("CSV Str utf-8: ", csv_str)
     df_ts = pd.read_csv(StringIO(csv_str), sep=",")
-    print("TS DF: ", df_ts)
+    #print("TS DF: ", df_ts)
     max_id = df_ts["ID"].max()
     min_id = df_ts["ID"].min()
+    """
     if not os.path.exists("./" + approach):
         os.mkdir("./" + approach)
-    for i in range(min_id, max_id):
+    #print("MIN ID: ", min_id)
+    #print("MAX ID: ", max_id)
+    for i in range(0, 1010):
         ts_time_command = "TS_SOCKET=/tmp/" + approach + " " + TS_EXECUTABLE + " -i " + str(i)
-        time_str = subprocess.check_output(ts_time_command, shell=True)
+        try:
+            time_str = subprocess.check_output(ts_time_command, shell=True)
+        except:
+            continue
+        print("Time str: ", time_str)
         if not os.path.isfile("./" + approach + "/" + str(i)):
             with open("./" + approach + "/" + str(i), "w") as f:
                 f.write(time_str.decode("utf-8"))
