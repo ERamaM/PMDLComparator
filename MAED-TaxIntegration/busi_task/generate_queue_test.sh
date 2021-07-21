1531828b0b24dbf85767dbb5a1a298df6b3bcd4b
efren.rama <efren.rama.maneiro@usc.es>
2021-07-21 10:23:45 +0200

    Add queue scripts for thai.
    Former-commit-id: f682d23db48181f3f2d33c82c78fefa9912e5f0a

---
+++ MAED-TaxIntegration/busi_task/generate_queue_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if command -v ts &> /dev/null
+then
+    TS_EXECUTABLE=ts
+else
+    TS_EXECUTABLE=tsp
+fi
+
+current_host=`hostname`
+DUMP_DIR=/home/efren.rama/tmp_hot_garbage/
+
+# Prepare to dump tmp files in other folder.
+if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
+then
+  mkdir -p $DUMP_DIR
+fi
+
+for i in $(ls ./data | grep  "train"); do
+	  log=${i/train_/}
+	  full_log=${i/train_fold[0-9]_variation[0-9]_/}
+	  if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
+	  then
+	    CUDA_VISIBLE_DEVICES=1 TMPDIR=$DUMP_DIR TS_SOCKET=/tmp/thai_test $TS_EXECUTABLE python bpi_run.py --fold_dataset ./data/$log --full_dataset ./data/$full_log --test
+	  else
+	    TS_SOCKET=/tmp/thai_test $TS_EXECUTABLE python bpi_run.py --fold_dataset ./data/$log --full_dataset ./data/$full_log --test
+	  fi
+done
