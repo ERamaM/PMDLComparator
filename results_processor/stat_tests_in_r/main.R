37bbba53e1cd24b626bf57e16bb2c42304dcf5d7
efren.rama <efren.rama.maneiro@usc.es>
2021-07-21 18:47:29 +0200

    Set up file processors.
    Former-commit-id: 2ca770d0805fbf70a85b81a45d4c7e8926cc0046

--- results_processor/stat_tests_in_r/main.R
+++ results_processor/stat_tests_in_r/main.R
@@ -1,5 +1,7 @@
+# R Version: 3.6.3
 #sudo apt install r-base
 #sudo apt install build-essential libcurl5-gnutls-dev libxml2-dev libssl-dev libv8-dev libgeos-dev
+
 if (!require("devtools")) {
   install.packages("devtools")
 }
@@ -19,22 +21,32 @@ if (!require("rstan")){
 if (!require("here")){
   install.packages("here")
 }
+if (!require("xtable")){
+  install.packages("xtable")
+}
 
 library("scmamp")
 library("ggplot2")
 library("here")
 
-data <- read.csv("processed_results/csv/acc.csv", row.names=1)
-print(data)
+if(dir.exists(file.path(getwd(), "results_processor/stat_tests_in_r"))){
+  setwd(file.path(getwd(), "results_processor/stat_tests_in_r"))
+}
+data_acc <- read.csv("../processed_results/csv/next_activity/results.csv", row.names=1)
 
-# In Demšar (2006) the author proposes a plot to visually check the differences,
-# the critical differece plot. This kind of plot can be created using the plotCD
-# function, which has two parameters, the data.
-# matrix and the significance level. In the plot, those algorithms that
-# are not joined by a line can be regarded as different.
-plotCD(data, alpha=0.05, cex=1.25)
+# Perform ranking with PlackettLuce
+ranking <- bPlackettLuceModel(data_acc, min=FALSE, nsim=5000, nchains=10, parallel=TRUE)
 
-data_bayesian <- bPlackettLuceModel(data, min=FALSE, nsim=5000, nchains=10, parallel=TRUE)
-print(data_bayesian$expected.win.prob)
-print(data_bayesian$expected.mode.rank)
-#plotDensities(data)
+sorted_probs <- data.frame(ranking$expected.win.prob)
+sorted_probs$approach <- rownames(sorted_probs)
+sorted_probs <- data.frame(sorted_probs[order(sorted_probs$ranking.expected.win.prob),])
+write.csv(sorted_probs, "../processed_results/csv/next_activity/plackett_sorted_probs.csv")
+print(xtable(sorted_probs, digits=4, caption="Approaches probability calculated by the Bayesian Plackett-Luce model"), file="../processed_results/latex/next_activity/plackett_sorted_probs.txt")
+
+sorted_rankings <- data.frame(ranking$expected.mode.rank)
+sorted_rankings$approach <- rownames(sorted_rankings)
+sorted_rankings <- data.frame(sorted_rankings[order(sorted_rankings$ranking.expected.mode.rank),])
+write.csv(sorted_rankings, "../processed_results/csv/next_activity/plackett_sorted_rankings.csv")
+print(xtable(sorted_rankings, digits=4, caption="Approaches probability calculated by the Bayesian Plackett-Luce model"), file="../processed_results/latex/next_activity/plackett_sorted_rankings.txt")
+
+# TODO: continue
