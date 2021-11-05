c0e420fe798fe3d9910f62cc1a1b1f23020005af
efren.rama <efren.rama.maneiro@usc.es>
2021-11-05 10:13:15 +0100

    Setup queue scripts.
    Former-commit-id: 9d904129dd36e7282e6184a82b76b75dd662728c

--- processtransformer/preprocess_all_datasets.sh
+++ processtransformer/preprocess_all_datasets.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+if command -v ts &> /dev/null
+then
+    TS_EXECUTABLE=ts
+else
+    TS_EXECUTABLE=tsp
+fi
+
+current_host=`hostname`
+DUMP_DIR=/home/efren.rama/tmp_hot_garbage/
+
+# Prepare to dump tmp files in other folder.
+if [ $current_host == "ctgpgpu8" ] || [ $current_host == "ctgpgpu7" ]
+then
+  mkdir -p $DUMP_DIR
+fi
+
 for i in $(ls ./datasets | grep -v "nasa"); do
   for j in $(ls ./datasets/$i/ | grep "train" ); do
     real_fold=${j/"train_"/}
-	  python data_processing.py --raw_log_file ./datasets/$i/$real_fold --task next_activity --dir_path ./datasets/$i/ --dataset $i
+	  TMPDIR=$DUMP_DIR TS_SOCKET=/tmp/zarahah_preprocess $TS_EXECUTABLE python data_processing.py --raw_log_file ./datasets/$i/$real_fold --task next_activity --dir_path ./datasets/$i/ --dataset $i
   done
 done
